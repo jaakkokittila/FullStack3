19e594eb7422d0019e7fae69b580f03e9d8882e0
Jaakko Kittilä <jmkitt@utu.fi>
2019-11-14 11:12:49 +0200

    Added build

---
+++ build/precache-manifest.53b6ead953e8be1efc19ec7b25efdc29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99d8c88459a61ab621a752b8e71d773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ec7fbc655e948a309b",
+    "url": "/static/css/main.5ceba802.chunk.css"
+  },
+  {
+    "revision": "9cabba44ff9439239689",
+    "url": "/static/js/2.3e63fad9.chunk.js"
+  },
+  {
+    "revision": "f4ec7fbc655e948a309b",
+    "url": "/static/js/main.8fae5eea.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
