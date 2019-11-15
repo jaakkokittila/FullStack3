a304fdf8e50fdc00b142b7de5e99440b837f6d6d
Jaakko Kittilä <jmkitt@utu.fi>
2019-11-15 16:46:05 +0200

    Commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 
+
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :response-time :url :status :post'))
 app.use(bodyParser.json())
